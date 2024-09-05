87f888603bb51dd21b0625ef37fc370e3cf4a7e1
hamzaclini <155651110+hamzaclini@users.noreply.github.com>
2024-09-05 10:09:14 +0200

    Update CLINICOG-Echantillonnage.py
    No password
    10 second before closing

--- CLINICOG-Echantillonnage.py
+++ CLINICOG-Echantillonnage.py
@@ -11,32 +11,32 @@ import requests
 
 st.set_page_config(page_title="CLINICOG Echantillonnage", page_icon="🧠")
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
+#def check_password():
+#    """Returns `True` if the user had the correct password."""
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the password.
-        else:
-            st.session_state["password_correct"] = False
+#    def password_entered():
+#        """Checks whether a password entered by the user is correct."""
+#        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+#            st.session_state["password_correct"] = True
+#            del st.session_state["password"]  # Don't store the password.
+#        else:
+#            st.session_state["password_correct"] = False
 
     # Return True if the password is validated.
-    if st.session_state.get("password_correct", False):
-        return True
+#    if st.session_state.get("password_correct", False):
+#        return True
 
     # Show input for password.
-    st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
-    )
-    if "password_correct" in st.session_state:
-        st.error("😕 Password incorrect")
-    return False
+#    st.text_input(
+#        "Password", type="password", on_change=password_entered, key="password"
+#    )
+#    if "password_correct" in st.session_state:
+#        st.error("😕 Password incorrect")
+#    return False
 
 
-if not check_password():
-    st.stop()  # Do not continue if check_password is not True.
+#if not check_password():
+#    st.stop()  # Do not continue if check_password is not True.
 
 @st.cache_resource
 def init_connection():
@@ -659,7 +659,7 @@ if button:
         write_data(document)
         st.write("### Merci d'avoir participé(e) à ce questionnaire. Ce questionnaire a été réalisé en collaboration avec Innov'Autonomie et GRHANDIOSE.")
         st.session_state.disabled = True
-        time.sleep(5)
+        time.sleep(10)
         st.rerun()
         
     elif all([participation == 'Oui', retrait == 'Oui', confidentialite == 'Oui', utilisation_donnees == 'Oui', ip_collection == 'Oui']):
